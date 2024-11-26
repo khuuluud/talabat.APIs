ff873582acba7b03652e6a7cbd1a6ca1a9d67a0b
DELL 5400 <DELL 5400@KHLUD>
2024-11-26 18:55:12 +0200

    Order Service Interface

---
+++ Talabat.Core/Services/IOrderService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entities.Order_aggregate;
+
+namespace Talabat.Core.Services
+{
+    public interface IOrderService
+    {
+        Task<Order> CreateOrderAsync(string BuyerEmail, string BasketId, int DeliveryMethod, Address ShippingAddress);
+
+        Task<IReadOnlyList<Order>> GetOrderForSpecificUserAsync(string BuyerEmail);
+
+        Task<Order> GetOrderForSpecificUserAsync(string BuyerEmail , int OrderId);
+
+    }
+}
---
+++ Talabat.Services/OrderService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entities.Order_aggregate;
+using Talabat.Core.Services;
+
+namespace Talabat.Services
+{
+    internal class OrderService : IOrderService
+    {
+        public Task<Order> CreateOrderAsync(string BuyerEmail, string BasketId, int DeliveryMethod, Address ShippingAddress)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<Order>> GetOrderForSpecificUserAsync(string BuyerEmail)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Order> GetOrderForSpecificUserAsync(string BuyerEmail, int OrderId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
